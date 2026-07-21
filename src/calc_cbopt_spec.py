01bdd30965aaec917afde4f2b914cf930de799b7
quantumfishswims <ericwfischer.sci@posteo.de>
2026-07-21 14:01:07 +0200

    calc_cbopt_spec

---
+++ src/calc_cbopt_spec.py
@@ -0,0 +1,63 @@
+#!/usr/bin/env python3
+
+from src.cbopt_vib_spec import CBOPTHessian
+
+
+def calcCBOPTirSpec(vib_modes,
+                    cav_modes,
+                    coupling,
+                    dip_deriv,  
+                    n_mol,
+                    polarizability,
+                    polarization,
+                    single_mode_approx,
+                    spec_grid,
+                    broadening,
+                    cbopt_order = None):
+    
+    cboptHessian = CBOPTHessian(vib_modes,
+                                cav_modes,
+                                coupling,
+                                dip_deriv,  
+                                n_mol,
+                                polarizability,
+                                polarization,
+                                single_mode_approx
+                                )
+    
+    if cbopt_order == "cbopt0_ir":
+        cbopt_eigensystem   = cboptHessian.build_cbopt0_hessian().eigensystem()
+        
+        cbopt_freqs         = cbopt_eigensystem.freqs
+                                    
+        cbopt_ir_spec       = cbopt_eigensystem.build_cbopt0_ir_spec().build_spec(spec_grid, 
+                                                                                    broadening=broadening, 
+                                                                                    cbopt_order="cbopt0_ir")
+        return cbopt_ir_spec, cbopt_freqs
+    
+    elif cbopt_order == "cbopt1_ir":
+        cbopt_eigensystem   = cboptHessian.build_cbopt1_hessian()\
+                                            .eigensystem()
+        
+        cbopt_freqs         = cbopt_eigensystem.freqs
+                                    
+        cbopt_ir_spec       = cbopt_eigensystem.build_cbopt1_ir_spec()\
+                                                .build_spec(spec_grid, 
+                                                            broadening=broadening, 
+                                                            cbopt_order="cbopt1_ir")
+        
+        return cbopt_ir_spec, cbopt_freqs
+        
+    elif cbopt_order == "cbopt2_ir":
+        cbopt_eigensystem   = cboptHessian.build_cbopt2_hessian()\
+                                            .eigensystem()
+        cbopt_freqs         = cbopt_eigensystem.freqs
+                                    
+        cbopt_ir_spec       = cbopt_eigensystem.build_cbopt2_ir_spec()\
+                                                .build_spec(spec_grid, 
+                                                            broadening=broadening, 
+                                                            cbopt_order="cbopt2_ir")
+        
+        return cbopt_ir_spec, cbopt_freqs
+        
+    
